b6b3dd1d988aa568e19812b8aefc4659b284f6f4
Gabe Levi <glevi@facebook.com>
2014-06-17 14:04:11 -0400

    Some vagrant fixes

--- puphpet/files/exec-always/setup-stuff.sh
+++ puphpet/files/exec-always/setup-stuff.sh
@@ -1,7 +1,13 @@
-#!/bash/bin
-git clone /var/copy_me ~/examples
+#!/bin/bash
+rm -rf ~vagrant/examples
+git clone /var/copy_me ~vagrant/examples
+chown -R vagrant:vagrant ~vagrant/examples
+
+# Install pathogen (need for the hack plugin)
+mkdir -p ~vagrant/.vim/autoload ~vagrant/.vim/bundle && curl -LSso ~vagrant/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo -e "\nexecute pathogen#infect()\n" >> ~vagrant/.vimrc
 
 # Set up vim hack plugin
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-echo "\nexecute pathogen#infect()"
-git clone git://github.com/hhvm/vim-hack.git ~/.vim/bundle/vim-hack
+rm -rf ~vagrant/.vim/bundle/vim-hack
+git clone git://github.com/hhvm/vim-hack.git ~vagrant/.vim/bundle/vim-hack
+chown -R vagrant:vagrant ~vagrant/.vim
